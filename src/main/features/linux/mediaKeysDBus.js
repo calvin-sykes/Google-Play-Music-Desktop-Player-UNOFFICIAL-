28a2a4b47d9766e2904eef4203acc88fa69ccc45
Calvin Sykes <sykescalvin09@gmail.com>
2018-03-15 00:47:30 +0000

    Use dbus-native for media key handling

--- src/main/features/linux/mediaKeysDBus.js
+++ src/main/features/linux/mediaKeysDBus.js
@@ -1,52 +1,52 @@
-import DBus from 'dbus';
+import { sessionBus } from 'dbus-native';
 
 function registerBindings(service, session) {
-  session.getInterface(service.bus_name, service.path, service.interface,
-  (err, iface) => {
-    if (!err) {
-      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
-        switch (keyName) {
-          case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
-          case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
-          case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
-          case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
-          default: return;
-        }
-      });
-      // FIXME: This should be called every time the window is focused
-      //        as this allows multiple applications to share control.
-      iface.GrabMediaPlayerKeys('google-play-music-desktop-player', 0); // eslint-disable-line
-      Logger.info(`Bound media keys for ${service.bus_name}`);
+  session.getService(service.bus_name).getInterface(
+    service.path,
+    service.interface,
+    (err, iface) => {
+      if (!err) {
+        iface.on('MediaPlayerKeyPressed', (n, keyName) => {
+          switch (keyName) {
+            case 'Next': Emitter.sendToGooglePlayMusic('playback:nextTrack'); return;
+            case 'Previous': Emitter.sendToGooglePlayMusic('playback:previousTrack'); return;
+            case 'Play': Emitter.sendToGooglePlayMusic('playback:playPause'); return;
+            case 'Stop': Emitter.sendToGooglePlayMusic('playback:stop'); return;
+            default: return;
+          }
+        });
+        // FIXME: This should be called every time the window is focused
+        //        as this allows multiple applications to share control.
+        iface.GrabMediaPlayerKeys('google-play-music-desktop-player', 0); // eslint-disable-line
+        Logger.info(`Bound media keys for ${service.bus_name}`);
+      }
     }
-  });
+  );
 }
 
 try {
-  const dbus = new DBus();
-  const session = dbus.getBus('session');
+  const session = sessionBus();
 
   const services = [
     {
       bus_name: 'org.gnome.SettingsDaemon.MediaKeys',
       path: '/org/gnome/SettingsDaemon/MediaKeys',
-      interface: 'org.gnome.SettingsDaemon.Mediakeys',
+      interface: 'org.gnome.SettingsDaemon.MediaKeys',
     },
     {
       bus_name: 'org.gnome.SettingsDaemon',
       path: '/org/gnome/SettingsDaemon/MediaKeys',
-      interface: 'org.gnome.SettingsDaemon.Mediakeys',
+      interface: 'org.gnome.SettingsDaemon.MediaKeys',
     },
     {
       bus_name: 'org.mate.SettingsDaemon',
       path: '/org/mate/SettingsDaemon/MediaKeys',
-      interface: 'org.mate.SettingsDaemon.Mediakeys',
+      interface: 'org.mate.SettingsDaemon.MediaKeys',
     },
   ];
 
   Logger.debug('Binding mediakeys');
-  for (service of services) {
-    registerBindings(service, session);
-  }
+  services.forEach((service) => { registerBindings(service, session); });
 } catch (e) {
   Logger.warn(`Failed to bind mediakeys: ${e}`);
 }
